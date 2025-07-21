3517470cd9a15769e62b18260cffe94be8ba26bf
itomti <itomti@proton.me>
2025-07-21 12:40:57 +0300

    move imports to utils

--- src/zelda/settings.py
+++ src/zelda/settings.py
@@ -1,9 +1,3 @@
-import logging
-from zelda.sprite.weapon import Weapon
-from zelda.sprite.spell import Spell
-from zelda.sprite.camera import YSortCameraGroup
-from zelda.config import Config
-
 WORLD_MAP = [
 ['x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x'],
 ['x',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','x'],
@@ -26,25 +20,3 @@ WORLD_MAP = [
 ['x',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','x'],
 ['x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x'],
 ]
-
-
-def import_weapon_data(camera: YSortCameraGroup) -> list[Weapon]:
-    logging.info("importing weapon data")
-    weapons: list[Weapon] = [
-        Weapon.create_axe(camera),
-        Weapon.create_lance(camera),
-        Weapon.create_rapier(camera),
-        Weapon.create_sai(camera),
-        Weapon.create_sword(camera)
-    ]
-    return weapons
-
-
-def import_magic_data(camera: YSortCameraGroup, config: Config) -> list[Spell]:
-    logging.info("importing magic data")
-    magic: list[Spell] = [
-        Spell.create_flame(camera, config),
-        Spell.create_heal(camera, config)
-    ]
-
-    return magic
--- src/zelda/utils.py
+++ src/zelda/utils.py
@@ -1,6 +1,8 @@
 import pygame
 import os
 from csv import reader
+from zelda.sprite.weapon import Weapon
+from zelda.sprite.spell import SpellType
 
 class Utilities:
     @staticmethod
@@ -23,3 +25,73 @@ class Utilities:
                 surface: pygame.Surface = pygame.image.load(full_path).convert_alpha()
                 surfaces.append(surface)
         return surfaces
+
+    @staticmethod
+    def import_player_assets() -> dict[str, list[pygame.Surface]]:
+        character_path = 'assets/graphics/player'
+        animations = {
+            'left': [],
+            'left_attack': [],
+            'left_idle': [],
+            'up': [],
+            'up_attack': [],
+            'up_idle': [],
+            'right': [],
+            'right_attack': [],
+            'right_idle': [],
+            'down': [],
+            'down_attack': [],
+            'down_idle': []
+        }
+
+        for animation in animations.keys():
+            folder_path: str = f"{character_path}/{animation}"
+            surfaces = Utilities.import_folder(folder_path)
+            animations[animation] = surfaces
+
+        return animations
+
+    @staticmethod
+    def import_weapon_data() -> list[dict]:
+        weapons: list[dict] = [
+            Weapon.create_axe(),
+            Weapon.create_lance(),
+            Weapon.create_rapier(),
+            Weapon.create_sai(),
+            Weapon.create_sword()
+        ]
+        return weapons
+
+
+    @staticmethod
+    def import_magic_data() -> list[dict]:
+        magic: list[dict] = []
+        spell_img: pygame.Surface = pygame.image.load('assets/graphics/particles/flame/fire.png').convert_alpha()
+        particles: list[pygame.Surface] = Utilities.import_folder('assets/graphics/particles/flame/frames')
+
+        data = {
+            'type': SpellType.FLAME,
+            'name': 'flame',
+            'image': spell_img,
+            'particles': particles,
+            'strength': 20,
+            'cost': 10
+        }
+
+        magic.append(data)
+
+        spell_img: pygame.Surface = pygame.image.load('assets/graphics/particles/heal/heal.png').convert_alpha()
+        particles: list[pygame.Surface] = Utilities.import_folder('assets/graphics/particles/heal/frames')
+
+        data = {
+            'type': SpellType.HEAL,
+            'name': 'heal',
+            'image': spell_img,
+            'particles': particles,
+            'strength': 5,
+            'cost': 10
+        }
+
+        magic.append(data)
+
+        return magic
