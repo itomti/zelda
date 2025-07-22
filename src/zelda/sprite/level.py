16b61c63eb5b47c4e077853731058335db0f1188
itomti <itomti@proton.me>
2025-07-23 00:26:36 +0300

    refactor level start

--- src/zelda/sprite/level.py
+++ src/zelda/sprite/level.py
@@ -1,9 +1,11 @@
+import logging
 import pygame
-
 from zelda.utils import Utilities
 from zelda.sprite.tile import Tile
 from zelda.sprite.player import Player
 from zelda.sprite.camera import YSortCameraGroup
+from zelda.sprite.spell import SpellInfo
+from zelda.sprite.enemy import Enemy, EnemyInfo
 from zelda.config import Config
 from zelda.ui.ui import UserInterface
 from random import choice
@@ -12,15 +14,15 @@ class Level:
     def __init__(self, config: Config, ui: UserInterface, display_surface: pygame.Surface, clock: pygame.time.Clock):
         self.clock: pygame.time.Clock = clock
         self.config = config
-        self.player: Player | None = None
+        self.player: Player
         self.display_surface = display_surface
         self.visible_sprites: pygame.sprite.Group = YSortCameraGroup()
         self.obstacle_sprites = pygame.sprite.Group()
-        self.weapon_data = Utilities.import_weapon_data()
-        self.magic_data = Utilities.import_magic_data()
+        self.weapon_data: list[dict]
+        self.magic_data: list[SpellInfo]
         self.create_map()
         self.ui = ui
-        self.monsters = Utilities.import_monster_data('assets/monsters')
+        self.monsters = []
 
     def run(self):
         self.visible_sprites.draw(self.player.rect)
@@ -29,6 +31,16 @@ class Level:
 
 
     def create_map(self):
+        animations = Utilities.import_player_assets()
+        image = pygame.image.load("assets/graphics/player/down/down_0.png").convert_alpha()
+        self.player = Player((2000, 1430), self.visible_sprites, self.obstacle_sprites, self.clock, self.config, animations, image)
+
+        self.weapon_data = Utilities.import_weapon_data()
+        self.magic_data = Utilities.import_magic_data()
+        self.monsters = Utilities.import_monster_data('assets/monsters')
+        m = self.monsters['raccoon']
+        enemy_info = EnemyInfo(m['name'], m['health'], m['experience'], m['damage'], m['attack_type'], m['attack_type'], m['image'])
+        self.monster = Enemy((2000, 1430), enemy_info, self.obstacle_sprites, self.visible_sprites)
         layouts = {
             'boundary': Utilities.import_csv_layout('assets/map/map_FloorBlocks.csv'),
             'grass': Utilities.import_csv_layout('assets/map/map_Grass.csv'),
@@ -54,6 +66,3 @@ class Level:
                     elif style == 'object':
                         object_surface = graphics['objects'][int(column)]
                         Tile(self.config, (x, y), [self.visible_sprites, self.obstacle_sprites], 'object', object_surface)
-
-        animations = Utilities.import_player_assets()
-        self.player = Player((2000, 1430), [self.visible_sprites], self.obstacle_sprites, self.clock, self.config, animations)
